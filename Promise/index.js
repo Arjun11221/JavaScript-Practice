ef08b71ec3a4d4dfa1f82efaf7de9bc9f9fb5997
Arjun <arjun346002@gmail.com>
2024-02-10 12:31:11 +0530

    promise with example

--- Promise/index.js
+++ Promise/index.js
@@ -1,1 +1,26 @@
-console.log("intro");
+const cart = ["Pants ", "Kurta", "Shirts"];
+
+createOrder(cart, (orderId) => {
+  proceedToPayment(orderId, (paymentInfo) => {
+    showOrderSummary(paymentInfo, (paymentInfo) => {
+      updateWalletBalance(paymentInfo);
+    });
+  });
+});
+
+// But Above Code has an issue of Inversion of Control
+
+// So we use Promise
+
+createOrder(cart)
+  .then((orderId) => proceedToPayment(orderId))
+  .then((paymentInfo) => showOrderSummary(paymentInfo))
+  .then((paymentInfo) => updateWalletBalance(paymentInfo));
+
+
+
+// const GITHUP_API = "https://api.github.com/users/Arjun11221";
+
+// const user = fetch(GITHUP_API);
+
+// console.log(user);
