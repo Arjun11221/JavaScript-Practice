5e07ba52480eb3a67ed729260a9ddce4c4492b0c
Arjun <arjun346002@gmail.com>
2024-02-21 23:09:13 +0530

    TDZ

---
+++ TDZ/app.js
@@ -0,0 +1,31 @@
+
+// console.log("aahsffw");
+// let a = 10;
+
+// console.log(a);
+// var b = 100;
+// var b = 1000;
+
+
+// console.log(b);
+
+// const a = 100;
+// const b = 10000;  
+
+// let a;
+// a = 10;
+// console.log(a);
+
+{
+    var a = 10;
+    let b = 20;
+    const c = 30;
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+console.log(a);
+console.log(b);
+console.log(c);
+
