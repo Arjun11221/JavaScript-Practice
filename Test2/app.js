edfbf235d6de44842477ee2760c1556a8b80bb54
Arjun <arjun346002@gmail.com>
2024-03-02 13:06:22 +0530

    practice

---
+++ Test2/app.js
@@ -0,0 +1,14 @@
+
+// function x(){
+//     console.log(a);
+//     a = 10;
+// }
+// x();
+// // console.log(a);
+
+foo();
+var foo = 20;
+function foo(){
+    console.log("Calling Foo");
+}
+foo();
