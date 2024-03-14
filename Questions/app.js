2c034c7f4f5a57297e420fd37238965fe1a93f71
Arjun <arjun346002@gmail.com>
2024-03-14 16:12:09 +0530

    coding practic

--- Questions/app.js
+++ Questions/app.js
@@ -87,18 +87,113 @@
 
 // console.log(newArr);
 
-function abc(str){
-    let occ = {};
-    str.split("").forEach((item)=>{
-        if(occ.hasOwnProperty(item)===false){
-            occ[item] = 1;
-        }
-        else {
-            occ[item]++;
-        }
-    })
-    return occ;
+// let str = "apple";
 
+// let obj = {};
+
+// str.split("").map((e)=>{
+//     if(obj.hasOwnProperty(e)===false){
+//         obj[e] = 1;
+//     }
+//     else {
+//         obj[e]++;
+//     }
+//     return obj;
+// })
+
+// console.log(obj);
+
+
+// const arr = [1,2,3,4,5];
+
+// let sum = 0;
+
+// // for(let i = 0; i<arr.length; i++){
+// //     sum += arr[i];
+// // }
+
+// arr.map((e)=>{
+//     sum += e;
+// })
+
+// console.log(sum);
+
+
+// const arr = ["asjc",12,"sdgvs","sbacxs",21,45,"aercx",10];
+
+// let sum = 0;
+// arr.forEach((elem)=>{
+//     if(typeof elem === "number"){
+//         sum += elem;
+//     }
+// });
+// console.log(sum);
+
+// const arr = [
+//     {
+//         name : "Arjun",
+//         age : 20,
+//         gender : "male"
+//     },
+//     {
+//         name : "karan",
+//         age : 20,
+//         gender : "male"
+//     },
+//     {
+//         name : "abc",
+//         age : 20,
+//         gender : "female"
+//     },
+//     {
+//         name : "xyz",
+//         age : 20,
+//         gender : "female"
+//     }
+// ];
+
+// let count = 0;
+// arr.forEach((e)=>{
+//     if(e.gender !== "male"){
+//         count++;
+//     }
+// });
+
+// for(let j = 0 ; j<=count; j++){
+//     for(let i = 0; i<arr.length; i++){
+//         if(arr[i].gender !== "male"){
+//             arr.splice(i,1);
+//         }
+//     }
+// }
+
+// console.log(arr);
+
+
+// const arr = [1,2,3,4];
+
+// let newArr = arr.map((e)=>{
+//     return e;
+// });
+
+// console.log(newArr);
+
+// function arr(arr,n=1){
+//     if(arr.length>=n){
+//         for(let i = 0; i<n; i++){
+//             console.log(arr[i]);
+//         }
+//     }
+// }
+
+// arr([1,2,3,4,5],5);
+
+function arr(arr,n=1){
+    if(arr.length>=n){
+        for(let i = 0; i<n; i++){
+            console.log(arr[arr.length-1-i]);
+        }
+    }
 }
 
-console.log(abc("abccsgavs"));
+arr([1,2,3,4,5],2);
