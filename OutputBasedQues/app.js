8842b162a8d181c587f63f42f5502a65dfe6c34b
Arjun <arjun346002@gmail.com>
2024-03-16 15:22:35 +0530

    practice

---
+++ OutputBasedQues/app.js
@@ -0,0 +1,18 @@
+// 1).
+
+let data = { name: "Arjun", age: 21 };
+let item = { city: "Delhi", skill: "JS" };
+
+data = {...data,...item};
+
+console.log(data);
+
+// 2).
+
+// let data = { name: "Arjun", age: 21 };
+// let item = { city: "Delhi", skill: "JS" };
+
+// data = {data,...item}; 
+
+// console.log(data);
+
